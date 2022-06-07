a94b262d00a2ee118f5cbf570ffc57ca91b1b6aa
alcoffeeocha <alkahfikhuzaimy@gmail.com>
2022-06-08 04:08:17 +0700

    added css reset

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 'use strict';
+import "./css/main.scss"
 
 if (module.hot) {
   module.hot.accept()
 }
 
-console.log('hello')
+const obj = {
+  a: 'a',
+  b: 'b',
+  c: {
+    cd: 'cd'
+  }
+}
+
+console.log(obj?.c?.cd,obj?.f?.fa)
